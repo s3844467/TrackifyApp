140a0306163c2f2485a0525f290df7be9fc596b4
s3844467 <s3844467@student.rmit.edu.au>
2022-02-21 00:54:07 +1100

    created basic layout of Trackify

---
+++ app.js
@@ -0,0 +1,37 @@
+// selectors
+const userInput = document.querySelector("#user-input");
+const submitButton = document.querySelector('#submit');
+const todoList = document.querySelector('.item_container');
+
+// event listeners
+submitButton.addEventListener('click', createToDo);
+submitButton.addEventListener('click', clearInput);
+
+// create a todo
+function createToDo() {
+    // creates new div
+    const todoDiv = document.createElement("div"); 
+    todoDiv.classList.add('todo'); 
+    // create item list
+    const newItem = document.createElement("li");
+    newItem.innerText = userInput.value; // todo-item becomes the value user types
+    newItem.classList.add('item'); 
+    todoDiv.appendChild(newItem); 
+    // create remove button
+    const removeButton = document.createElement('button'); 
+    removeButton.innerText = 'remove'; 
+    removeButton.classList.add('removebtn'); 
+    newItem.appendChild(removeButton);
+    todoList.appendChild(todoDiv); // adds tododiv to main container
+    // remove item from list
+    removeButton.addEventListener('click', () => {
+        newItem.innerText = '';
+    })
+}
+
+// clear text input
+function clearInput() {
+    userInput.value = '';
+}
+
+

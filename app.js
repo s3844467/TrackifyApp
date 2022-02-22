0310f164d1096d2a212acc9f2168587cd2ceff29
Ujwal Mamidi s3844467 <s3844467@student.rmit.edu.au>
2022-02-22 17:06:55 +1100

    added styling to the app layout

--- app.js
+++ app.js
@@ -12,17 +12,31 @@ function createToDo() {
     // creates new div
     const todoDiv = document.createElement("div"); 
     todoDiv.classList.add('todo'); 
+
     // create item list
     const newItem = document.createElement("li");
-    newItem.innerText = userInput.value; // todo-item becomes the value user types
+    newItem.innerText = userInput.value; // todo-item becomes the value user enters
     newItem.classList.add('item'); 
     todoDiv.appendChild(newItem); 
+
+    // create completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerText = 'completed';
+    completedButton.classList.add('completedbtn');
+    newItem.appendChild(completedButton);
+
     // create remove button
     const removeButton = document.createElement('button'); 
     removeButton.innerText = 'remove'; 
     removeButton.classList.add('removebtn'); 
     newItem.appendChild(removeButton);
     todoList.appendChild(todoDiv); // adds tododiv to main container
+
+    // dash line through item when completed
+    completedButton.addEventListener('click', () => {
+        newItem.classList.toggle('completed');
+    })
+
     // remove item from list
     removeButton.addEventListener('click', () => {
         newItem.innerText = '';
@@ -32,6 +46,6 @@ function createToDo() {
 // clear text input
 function clearInput() {
     userInput.value = '';
-}
+} 
 
 
